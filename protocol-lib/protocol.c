743d5aa56d81c3e7cb393da42e734490460067b3
julien <ju@pop-os.localdomain>
2024-03-23 19:53:01 +0100

    [protocol-lib] protocol: Finish and expose `protocol_encode()`

--- protocol-lib/protocol.c
+++ protocol-lib/protocol.c
@@ -1,13 +1,19 @@
+#include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 #include <stdio.h>
 
 #include "base64.h"
 
 #include "protocol.h"
 
-int protocol_encode(struct protocol_frame *p_frame) {
+int protocol_encode(struct protocol_frame *p_frame, char *out_encoded_buffer, size_t encoded_buffer_size) {
   if (p_frame == NULL) {
     return PROTOCOL_ERROR_NO_FRAME;
+  }
+
+  if (out_encoded_buffer== NULL) {
+    return PROTOCOL_ERROR_NO_OUT;
   }
 
   if (p_frame->data == NULL) {
@@ -15,6 +21,19 @@ int protocol_encode(struct protocol_frame *p_frame) {
   }
 
   uint8_t raw_data[512] = { 0 };
+  size_t raw_data_size = 10 + p_frame->data_size;
+  memcpy(raw_data, p_frame, 10);
+  memcpy(&raw_data[10], p_frame->data, p_frame->data_size);
+
+  int encoded_frame_len = 0;
+  char *encoded_frame = base64(raw_data, raw_data_size, &encoded_frame_len);
+  memset(out_encoded_buffer, 0, encoded_buffer_size);
+  if (encoded_buffer_size <= encoded_frame_len) {
+    free(encoded_frame);
+    return PROTOCOL_ERROR_OUT_TOO_SMALL;
+  }
+  memcpy(out_encoded_buffer, encoded_frame, encoded_frame_len);
+  free(encoded_frame);
 
   return PROTOCOL_OK;
 }
--- protocol-lib/protocol.h
+++ protocol-lib/protocol.h
@@ -7,7 +7,9 @@ enum PROTOCOL_RESULTS {
   PROTOCOL_NOK = 1,
   PROTOCOL_ERROR = -1,
   PROTOCOL_ERROR_NO_FRAME = -2,
-  PROTOCOL_ERROR_NO_DATA = -3,
+  PROTOCOL_ERROR_NO_OUT = -3,
+  PROTOCOL_ERROR_NO_DATA = -4,
+  PROTOCOL_ERROR_OUT_TOO_SMALL = -5,
 };
 
 struct protocol_frame {
@@ -16,3 +18,5 @@ struct protocol_frame {
   uint8_t data_size;
   uint8_t *data;
 } __attribute__((packed)); ;
+
+int protocol_encode(struct protocol_frame *p_frame, char *out_encoded_buffer, size_t encoded_buffer_size);
