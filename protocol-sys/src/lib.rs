1546707bf79b3a183a3befea2ed230666fed35da
julien <ju@pop-os.localdomain>
2024-03-30 20:50:22 +0100

    [protocol-sys] lib.rs: Add implementation for car's TryFrom

--- protocol-sys/src/lib.rs
+++ protocol-sys/src/lib.rs
@@ -3,3 +3,16 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+impl TryFrom<protocol_frame> for car {
+    type Error = &'static str;
+
+    fn try_from(value: protocol_frame) -> Result<Self, Self::Error> {
+        if value.id == PROTOCOL_CAR.try_into().unwrap_or_default() {
+            return Ok(Self {
+                ..Default::default()
+            });
+        }
+        Err("Expected self.id = PROTOCOL_CAR, got: {value.id}")
+    }
+}
