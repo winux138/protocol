8e2a65491181fc83e7bb0d42a5664c79f3da0add
julien <ju@pop-os.localdomain>
2024-03-30 22:11:39 +0100

    [protocol-sys] lib.rs: Add implementation for animal's TryFrom

--- protocol-sys/src/lib.rs
+++ protocol-sys/src/lib.rs
@@ -4,6 +4,19 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+impl TryFrom<protocol_frame> for animal {
+    type Error = &'static str;
+
+    fn try_from(value: protocol_frame) -> Result<Self, Self::Error> {
+        if value.id == PROTOCOL_ANIMAL.try_into().unwrap_or_default() {
+            return Ok(Self {
+                ..Default::default()
+            });
+        }
+        Err("Expected self.id = PROTOCOL_ANIMAL, got: {value.id}")
+    }
+}
+
 impl TryFrom<protocol_frame> for car {
     type Error = &'static str;
 
