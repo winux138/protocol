f971d64e761fa8ea82c59d2930d4fa851fe22f23
julien <ju@pop-os.localdomain>
2024-04-07 18:16:20 +0200

    [app-in-rust] main.rs: Add POC for decode + TryInto

--- app-in-rust/src/main.rs
+++ app-in-rust/src/main.rs
@@ -9,7 +9,13 @@ fn main() {
     };
 
     if let Some(encoded_frame) = protocol_rs::encode(&mut animal) {
-        println!("Encoded frame is: '{encoded_frame}'");
+        println!("Encoded frame is: '{encoded_frame}' ({len})", len = encoded_frame.len());
+        let decoded_frame = protocol_rs::ProtocolFrame::new(&encoded_frame);
+        println!("ProtocolFrame is of id: {}", decoded_frame.id());
+        if let Ok(animal) = TryInto::<protocol_rs::animal>::try_into(decoded_frame) {
+            println!("I have an animal!");
+            println!("{:?}", animal);
+        }
     } else {
         println!("Could not encode the frame");
     }
