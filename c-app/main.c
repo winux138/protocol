afafff312105b7e7205150b885a0d3cd81ab2be1
julien <ju@pop-os.localdomain>
2024-04-17 21:06:28 +0200

    [c-app] main.c: Add a name to our animal + use new decode in main

--- c-app/main.c
+++ c-app/main.c
@@ -10,7 +10,7 @@ void car_handler(struct car *p_car);
 
 int main(void) {
         struct animal animal = {
-                .name = "",
+                .name = "Bob",
                 .age = 1,
                 .legs = 2,
         };
@@ -24,19 +24,7 @@ int main(void) {
 
         printf("Encoded frame is: '%s'\n", encoded_frame);
 
-        struct animal frame2_data;
-        struct protocol_frame frame2 = {.data = (uint8_t *)&frame2_data,
-                .data_size = sizeof(frame2_data)};
-        protocol_decode(&frame2, encoded_frame, strnlen(encoded_frame, 512));
-
-        printf("decoded_frame {\n");
-        printf("    .id: %d\n", frame2.id);
-        printf("    .ts: %ld\n", frame2.ts);
-        printf("    .data_size: %d\n", frame2.data_size);
-        printf("    .data.name: %s\n", ((struct animal *)frame2.data)->name);
-        printf("    .data.age: %d\n", ((struct animal *)frame2.data)->age);
-        printf("    .data.legs: %d\n", ((struct animal *)frame2.data)->legs);
-        printf("}\n");
+        protocol_decode(decoded_callback, encoded_frame, strnlen(encoded_frame, 512));
 
         return 0;
 }
