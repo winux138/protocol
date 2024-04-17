9823bcd99ab519202d66a96d34a5da2b64469727
julien <ju@pop-os.localdomain>
2024-04-17 20:43:24 +0200

    [c-app] main.c: Format + use 8 spaces

--- c-app/main.c
+++ c-app/main.c
@@ -5,34 +5,34 @@
 #include "protocol-lib/protocol.h"
 
 int main(void) {
-  struct animal animal = {
-      .name = "",
-      .age = 1,
-      .legs = 2,
-  };
-  struct protocol_frame frame = {.id = 1,
-                                 .ts = 1337,
-                                 .data_size = sizeof(animal),
-                                 .data = (uint8_t *)&animal};
+        struct animal animal = {
+                .name = "",
+                .age = 1,
+                .legs = 2,
+        };
+        struct protocol_frame frame = {.id = 1,
+                .ts = 1337,
+                .data_size = sizeof(animal),
+                .data = (uint8_t *)&animal};
 
-  char encoded_frame[512];
-  protocol_encode(&frame, encoded_frame, 512);
+        char encoded_frame[512];
+        protocol_encode(&frame, encoded_frame, 512);
 
-  printf("Encoded frame is: '%s'\n", encoded_frame);
+        printf("Encoded frame is: '%s'\n", encoded_frame);
 
-  struct animal frame2_data;
-  struct protocol_frame frame2 = {.data = (uint8_t *)&frame2_data,
-                                  .data_size = sizeof(frame2_data)};
-  protocol_decode(&frame2, encoded_frame, strnlen(encoded_frame, 512));
+        struct animal frame2_data;
+        struct protocol_frame frame2 = {.data = (uint8_t *)&frame2_data,
+                .data_size = sizeof(frame2_data)};
+        protocol_decode(&frame2, encoded_frame, strnlen(encoded_frame, 512));
 
-  printf("decoded_frame {\n");
-  printf("    .id: %d\n", frame2.id);
-  printf("    .ts: %ld\n", frame2.ts);
-  printf("    .data_size: %d\n", frame2.data_size);
-  printf("    .data.name: %s\n", ((struct animal *)frame2.data)->name);
-  printf("    .data.age: %d\n", ((struct animal *)frame2.data)->age);
-  printf("    .data.legs: %d\n", ((struct animal *)frame2.data)->legs);
-  printf("}\n");
+        printf("decoded_frame {\n");
+        printf("    .id: %d\n", frame2.id);
+        printf("    .ts: %ld\n", frame2.ts);
+        printf("    .data_size: %d\n", frame2.data_size);
+        printf("    .data.name: %s\n", ((struct animal *)frame2.data)->name);
+        printf("    .data.age: %d\n", ((struct animal *)frame2.data)->age);
+        printf("    .data.legs: %d\n", ((struct animal *)frame2.data)->legs);
+        printf("}\n");
 
-  return 0;
+        return 0;
 }
